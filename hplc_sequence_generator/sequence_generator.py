cd6f28474e867ab51fbf2520bf83dc3cc265543d
Dimitris Gavriilidis <102852721+dimitris-gavriilidis@users.noreply.github.com>
2025-08-28 18:55:06 +0300

    Update sequence_generator.py

--- hplc_sequence_generator/sequence_generator.py
+++ hplc_sequence_generator/sequence_generator.py
@@ -4,7 +4,7 @@ def hplc_sequence_generator():
     print("🔹 Welcome to the HPLC Sequence Generator 🔹")
     
     try:
-        n = int(input("Enter number of samples to generate: "))
+        n = int(input("Enter number of samples to generatee: "))
         for i in range(1, n + 1):
             print(f"Sample {i}")
     
